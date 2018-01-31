eab189cbf6f87fffe277a6972cccb615560a4b35
Gabriele Tassoni <gabriele.tassoni@gmail.com>
2018-01-31 22:41:42 +0100

    Added static assets manager

--- lib/thecore_dataentry_with_date_start_and_end.rb
+++ lib/thecore_dataentry_with_date_start_and_end.rb
@@ -1,3 +1,6 @@
+require 'thecore_dataentry_commons'
+require 'thecore_ui_partial_snippets'
+
 require "thecore_dataentry_with_date_start_and_end/engine"
 
 module ThecoreDataentryWithDateStartAndEnd
--- lib/thecore_dataentry_with_date_start_and_end/engine.rb
+++ lib/thecore_dataentry_with_date_start_and_end/engine.rb
@@ -1,4 +1,11 @@
 module ThecoreDataentryWithDateStartAndEnd
   class Engine < ::Rails::Engine
+    # This code makes the static assets present in this 
+    # engine available to main app, even if this gem is 
+    # just a dependency of another engine
+    initializer 'thecore_dataentry_with_date_start_and_end.load_static_assets' do |app|
+      puts "Loading static assets for #{root}"
+      app.middleware.use ::ActionDispatch::Static, "#{root}/app"
+    end
   end
 end
