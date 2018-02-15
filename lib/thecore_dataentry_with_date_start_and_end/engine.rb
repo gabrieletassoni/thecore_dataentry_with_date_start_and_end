3dc41751e9044b69682ed2da04cf4c324642acaf
Gabriele Tassoni <gabriele.tassoni@gmail.com>
2018-02-15 03:22:07 +0100

    Management of switches for datetime creation and options

--- lib/thecore_dataentry_with_date_start_and_end/engine.rb
+++ lib/thecore_dataentry_with_date_start_and_end/engine.rb
@@ -3,9 +3,9 @@ module ThecoreDataentryWithDateStartAndEnd
     # This code makes the static assets present in this 
     # engine available to main app, even if this gem is 
     # just a dependency of another engine
-    initializer 'thecore_dataentry_with_date_start_and_end.load_static_assets' do |app|
-      puts "Loading static assets for #{root}"
-      app.middleware.use ::ActionDispatch::Static, "#{root}/app"
-    end
+    # initializer 'thecore_dataentry_with_date_start_and_end.load_static_assets' do |app|
+    #   puts "Loading static assets for #{root}"
+    #   app.middleware.use ::ActionDispatch::Static, "#{root}/app"
+    # end
   end
 end
--- lib/thecore_dataentry_with_date_start_and_end/version.rb
+++ lib/thecore_dataentry_with_date_start_and_end/version.rb
@@ -1,3 +1,3 @@
 module ThecoreDataentryWithDateStartAndEnd
-  VERSION = '1.1.3'
+  VERSION = '1.1.6'.freeze
 end
