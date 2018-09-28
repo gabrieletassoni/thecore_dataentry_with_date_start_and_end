3dfd830b40fe8cbcbff9a37c3c3e8d0179f642e3
Gabriele Tassoni <gabriele.tassoni@gmail.com>
2018-09-28 16:25:52 +0200

    Remove debug code

--- lib/thecore_dataentry_with_date_start_and_end/engine.rb
+++ lib/thecore_dataentry_with_date_start_and_end/engine.rb
@@ -4,7 +4,7 @@ module ThecoreDataentryWithDateStartAndEnd
     # engine available to main app, even if this gem is 
     # just a dependency of another engine
     # initializer 'thecore_dataentry_with_date_start_and_end.load_static_assets' do |app|
-    #   puts "Loading static assets for #{root}"
+    #   # puts "Loading static assets for #{root}"
     #   app.middleware.use ::ActionDispatch::Static, "#{root}/app"
     # end
   end
